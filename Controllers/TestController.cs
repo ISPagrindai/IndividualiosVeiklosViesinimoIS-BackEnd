4b61c0ff7149aef5da291dd4950987c3f77dd591
domantaskernagis <58690155+domantaskernagis@users.noreply.github.com>
2020-12-19 16:17:34 +0200

    bruv momento

---
+++ Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using is_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController
+    {
+        private readonly individuali_veiklaContext _context;
+
+        public TestController(individuali_veiklaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Vieta> Get()
+        {
+            return _context.Vieta.ToList();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using is_backend.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,7 @@ namespace is_backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
+            services.AddDbContext<individuali_veiklaContext>(options => options.UseMySql(Configuration.GetConnectionString("IndividualiVeiklaDB")));
             services.AddSwaggerGen();
         }
 
