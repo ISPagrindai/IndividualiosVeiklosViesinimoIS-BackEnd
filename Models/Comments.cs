041250f169d39c83d31a79c70df318d8b95c896f
domantaskernagis <58690155+domantaskernagis@users.noreply.github.com>
2020-12-19 15:36:19 +0200

    database-first added

---
+++ Models/Comments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace is_backend.Models
+{
+    public partial class Comments
+    {
+        public int IdComments { get; set; }
+        public string Turinys { get; set; }
+    }
+}
---
+++ Models/individuali_veiklaContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace is_backend.Models
+{
+    public partial class individuali_veiklaContext : DbContext
+    {
+        public individuali_veiklaContext()
+        {
+        }
+
+        public individuali_veiklaContext(DbContextOptions<individuali_veiklaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Comments> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("user id=root;persistsecurityinfo=True;server=127.0.0.1;database=individuali_veikla;password=root", x => x.ServerVersion("8.0.20-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comments>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("comments");
+
+                entity.Property(e => e.IdComments).HasColumnName("idComments");
+
+                entity.Property(e => e.Turinys)
+                    .HasColumnName("turinys")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
