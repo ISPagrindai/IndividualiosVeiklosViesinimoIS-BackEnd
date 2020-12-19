ae754edc3dd539fa77666611fca2e7293786c119
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-12-19 13:46:36 +0200

    Added swagger

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,8 @@ namespace is_backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,12 @@ namespace is_backend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1");
+                });
             }
 
             app.UseHttpsRedirection();
