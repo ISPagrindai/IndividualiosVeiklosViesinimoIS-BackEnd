6f278ea564d945d42d3ff879839efc91eb875f07
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-12-20 15:44:23 +0200

    Added helper controller

---
+++ Controllers/HelperController.cs
@@ -0,0 +1,34 @@
+﻿using is_backend.Dto;
+using is_backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelperController : ControllerBase
+    {
+        private readonly individuali_veiklaContext _db;
+
+        public HelperController(individuali_veiklaContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<GET_VeiklosTipas> getJobTypes()
+        {
+            var temp = _db.VeiklosTipas.Select(a =>  mapToGet(a)).ToHashSet();
+            return temp;
+        }
+        private static GET_VeiklosTipas mapToGet(VeiklosTipas a)
+        {
+            return new GET_VeiklosTipas() { Id = a.IdVeiklosTipas, Pavadinimas = a.Pavadinimas };
+        }
+    }
+}
---
+++ Dto/GET_VeiklosTipas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is_backend.Dto
+{
+    public class GET_VeiklosTipas
+    {
+        public string Pavadinimas { get; set; }
+
+        public int Id { get; set; }
+    }
+}
