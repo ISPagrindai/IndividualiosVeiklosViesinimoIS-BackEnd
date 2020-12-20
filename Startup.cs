755f370d1d831a303685c312e571e75aa82a3f84
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-12-20 14:20:43 +0200

    Cors and other stuff

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace is_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddDbContext<individuali_veiklaContext>(options => options.UseMySql(Configuration.GetConnectionString("IndividualiVeiklaDB")));
             services.AddSwaggerGen();
@@ -61,6 +62,12 @@ namespace is_backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
