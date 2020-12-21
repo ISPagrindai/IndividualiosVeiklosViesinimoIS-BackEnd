7362385f888221ce2bb3f607bd5ceba8e6dca999
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-12-21 22:43:14 +0200

    a

---
+++ Dto/GET_Kandidatas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is_backend.Dto
+{
+    public class GET_Kandidatas
+    {
+        public int Id { get; set; }
+        public string Vardas {get; set;}
+
+        public string Pavarde { get; set; }
+
+        public string Lytis { get; set; }
+
+        public string Epastas { get; set; }
+
+        public DateTime? GimimoData { get; set; }
+    }
+}
---
+++ Dto/PUT_Imone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is_backend.Dto
+{
+    public class PUT_Imone
+    {
+        [Required]
+        public bool ArUzsaldytas { get; set; }
+        [Required]
+        public int IdImone { get; set; }
+
+    }
+}
